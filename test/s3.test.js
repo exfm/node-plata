1c9feef7e61445dca0f7ebcbee2ea5534c629d9e
Lucas Hrabovsky <lucas@ex.fm>
2012-11-17 11:44:53 -0500

    Rewrite S3 to actually make sense.

--- test/s3.test.js
+++ test/s3.test.js
@@ -12,55 +12,23 @@ describe("S3", function(){
     describe("key", function(){
         it("should create a new key");
         it("should be able to do HEAD lookups", function(done){
-            aws.s3.lookup('exfmnodetest', '1.json').then(function(headers){
+            aws.s3.lookup('exfmnodetest', '1.json').then(function(res){
                 done();
-            });
+            }, done);
         });
         it("should be able to do HEAD lookups for non-existent keys", function(done){
-            aws.s3.lookup('exfmnodetest', '2.json').then(function(headers){
+            aws.s3.lookup('exfmnodetest', '2.json').then(function(res){
+                done(new Error("Should have been rejected"));
+            }, function(){
                 done();
             });
         });
-        it("should be return false for non-existent keys", function(done){
-            aws.s3.objectExists('exfmnodetest', '2.json').then(function(exists){
-                assert.equal(exists, false);
+        it("should be reject for non-existent keys", function(done){
+            aws.s3.exists('exfmnodetest', '2.json').then(function(res){
+                done(new Error("Should have been rejected."));
+            }, function(){
                 done();
             });
         });
     });
-
 });
-
-// sequence(aws).then(
-//     function(next){
-//         aws.s3.createBucket('exfmnodetest').then(
-//             function(r){
-//                 next();
-//             },
-//             function(err){
-//                 console.error(err);
-//             }
-//         );
-//     }).then(
-//     function(next){
-//         aws.s3.putObject('exfmnodetest', '1.json', JSON.stringify({'hello': 'world'})).then(
-//             function(r){
-//                 console.log(r);
-//                 next();
-//             },
-//             function(err){
-//                 console.error(err);
-//             }
-//         );
-//     }).then(
-//     function(next){
-//         aws.s3.listKeys('exfmnodetest').then(
-//             function(r){
-//                 console.log(r);
-//             },
-//             function(err){
-//                 console.error(err);
-//             }
-//         );
-//     });
-
